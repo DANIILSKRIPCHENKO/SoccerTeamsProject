f041a47374d7e385a9edf1b60abbd9c33c17ad54
”Daniil <das76@tpu.ru>
2021-10-30 07:03:24 +0700

    Add project files.

---
+++ SoccerTeamsProject/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SoccerTeamsProject.Models;
+using System.Data.Entity;
+
+namespace SoccerTeamsProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        SoccerContext db = new SoccerContext();
+
+        // Выводим всех футболистов
+        public ActionResult Index()
+        {
+            var players = db.Players.Include(p => p.Team);
+            return View(players.ToList());
+        }
+
+        public ActionResult TeamDetails(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Team team = db.Teams.Include(t => t.Players).FirstOrDefault(t => t.Id == id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+    }
+}
